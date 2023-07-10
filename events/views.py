0205fffdbd576051e6e89f367a85a191b9c48567
Andreas Karlsson <andreas.krln@hotmail.com>
2023-07-10 18:18:47 +0000

    Changed events and profile views to generics

--- events/views.py
+++ events/views.py
@@ -1,69 +1,26 @@
 from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
 from explore_sthlm_api.permissions import IsOwnerOrReadOnly
 
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     """ List all events and checks permissions """
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [ 
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def get(self, request):
-        events = Event.objects.all()
-        serializer = EventSerializer(
-            events, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = EventSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-        
-class EventDetail(APIView):
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     """ 
-    Shows detailsviews of events, also handling error codes.
-    Let's you get, edit and delete events
+    Shows details views of events, also handling error codes.
+    Let's you get, edit, and delete events
     """
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    
-    def get_object(self, pk):
-        try:
-            event = Event.objects.get(pk=pk)
-            self.check_object_permissions(self.request, event)
-            return event
-        except Event.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(event, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(event, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        event = self.get_object(pk)
-        event.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -7,47 +7,21 @@ from .serializers import ProfileSerializer
 from explore_sthlm_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
     No need to use a create view cause profiles are created when a user is
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    
-class ProfileDetail(APIView):
-    """ 
-    Shows detailsviews of profile, also handling error codes.
-    Let's you get, edit and delete profile
-    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        profile = self.get_object(pk)
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """ 
+    Shows details views of a profile, also handling error codes.
+    Let's you get, edit, and delete a profile
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
