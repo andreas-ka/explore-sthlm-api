75f5b78cfb89c3b62dec12827a4653ac77d9c2f9
Andreas Karlsson <andreas.krln@hotmail.com>
2023-07-13 09:02:01 +0000

    Changed if Client origin dev in settings.py to match CI tutorial

--- explore_sthlm_api/settings.py
+++ explore_sthlm_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -56,13 +57,23 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-andreaska-exploresthlma-06w5yaz8e8v.ws-eu101.gitpod.io', 'explore-sthlm.herokuapp.com', 'explore-sthlm-231a49fd8022.herokuapp.com']
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -106,15 +117,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-     ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
-CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'explore_sthlm_api.urls'
 
