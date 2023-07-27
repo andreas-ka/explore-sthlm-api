9e1534a604a13722c7e1492e2a669cf0970d2704
Andreas Karlsson <andreas.krln@hotmail.com>
2023-07-27 18:00:10 +0000

    Added google maps API

--- explore_sthlm_api/settings.py
+++ explore_sthlm_api/settings.py
@@ -57,6 +57,7 @@ JWT_AUTH_SAMESITE = 'None'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+REACT_APP_GOOGLE_MAPS_API_KEY = os.environ.get('REACT_APP_GOOGLE_MAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
